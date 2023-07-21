864ae3dfaf5ed311f0f3fbe4dbd930d60dba6039
Cleazyy <cleazy2002@gmail.com>
2023-07-21 19:06:43 +0200

    fix client

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -6,11 +6,15 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 17:59:23 by fluchten          #+#    #+#             */
-/*   Updated: 2023/07/21 19:03:49 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/07/21 19:05:57 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Client.hpp"
+
+/* ************************************************************************** */
+/*                               Canonical form                               */
+/* ************************************************************************** */
 
 Client::Client(Parser &config, Server &server, std::vector<Location> &location) : _config(config), _server(server), _location(location)
 {
@@ -33,44 +37,42 @@ Client::Client(Parser &config, Server &server, std::vector<Location> &location) 
 	_socketFd = 0;
 }
 
-Client::Client(const Client &srcs) :
-    _config(srcs._config),
-	_server(srcs._server),
-	_location(srcs._location)
-{ *this = srcs; }
+Client::Client(const Client &rhs) : _config(rhs._config), _server(rhs._server), _location(rhs._location)
+{
+	*this = rhs;
+}
+
+Client &Client::operator=(const Client &rhs)
+{
+	if (this != &rhs) {
+		_socketFd = rhs._socketFd;
+		_socketAddress = rhs._socketAddress;
+		_socketAddrLen = rhs._socketAddrLen;
+		_isAlive = rhs._isAlive;
+		_requestStr << rhs._requestStr.str();
+		_contentLenght = rhs._contentLenght;
+		_bodySize = rhs._bodySize;
+		_requestPars = rhs._requestPars;
+		_httpVersion = rhs._httpVersion;
+		_uri = rhs._uri;
+		_method = rhs._method;
+		_headers = rhs._headers;
+		_bodyReq << rhs._bodyReq.str();
+		_cookie = rhs._cookie;
+		_query = rhs._query;
+		_filePath = rhs._filePath;
+		_bodyRep = rhs._bodyRep;
+		_response = rhs._response;
+		_sizeSend = rhs._sizeSend;
+		_sizeRep = rhs._sizeRep;
+		_config = rhs._config;
+		_server = rhs._server;
+		_location = rhs._location;
+	}
+	return (*this);
+}
 
 Client::~Client(void)
 {
 	return ;
-}
-
-Client &Client::operator=(const Client &srcs)
-{
-	if (this != &srcs)
-	{
-		_socketFd = srcs._socketFd;
-		_socketAddress = srcs._socketAddress;
-		_socketAddrLen = srcs._socketAddrLen;
-		_isAlive = srcs._isAlive;
-		_requestStr << srcs._requestStr.str();
-		_contentLenght = srcs._contentLenght;
-		_bodySize = srcs._bodySize;
-		_requestPars = srcs._requestPars;
-		_httpVersion = srcs._httpVersion;
-		_uri = srcs._uri;
-		_method = srcs._method;
-		_headers = srcs._headers;
-		_bodyReq << srcs._bodyReq.str();
-		_cookie = srcs._cookie;
-		_query = srcs._query;
-		_filePath = srcs._filePath;
-		_bodyRep = srcs._bodyRep;
-		_response = srcs._response;
-		_sizeSend = srcs._sizeSend;
-		_sizeRep = srcs._sizeRep;
-		_config = srcs._config;
-		_server = srcs._server;
-		_location = srcs._location;
-	}
-	return (*this);
 }
